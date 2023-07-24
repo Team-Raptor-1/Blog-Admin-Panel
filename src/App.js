a23ca5b0e875e87706397e4472454bcc76e7e8c4
Hashim66 <138629892+Hashim66@users.noreply.github.com>
2023-07-24 15:04:58 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminSettingsPage from './component/userAndpass';
+
+function App (){
+  return (
+    <div>
+      <AdminSettingsPage/>
+    </div>
+  );
+};
+
+export default App;
